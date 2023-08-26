f4db439a70a4def31b683c09d0b7673bbcd0c25b
MuscuarBean <email@example.com>
2023-08-25 19:42:11 -0700

    Exercise Completed

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,38 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite subject in school?");
+            string userInput = Console.ReadLine();
+
+            switch (userInput.ToLower())
+            {
+
+                case "math":
+                    Console.WriteLine("You must be smart.");
+                    break;
+
+                case "english":
+                    Console.WriteLine("You like to write essays?");
+                    break;
+
+                case "science":
+                    Console.WriteLine("The mitochindria is the powerhouse of the cell.");
+                    break;
+                case "art":
+                    Console.WriteLine("Ceramics was fun. You ever take that class?");
+                    break;
+                case "music":
+                    Console.WriteLine("You were forced the play the recorder at some point right?");
+                    break;
+                default:
+                    Console.WriteLine("I have never heard of that...");
+                    break;
+
+            }
+            
+
+
+
         }
     }
 }
